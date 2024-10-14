7c6863ecbc03fe10ec2f51c90b3fc9314ffd43f6
a106857 <a106857@alunos.uminho.pt>
2024-10-14 01:04:19 +0100

    Adicionadas funções da entidade Music.

--- trabalho-pratico/include/entities/musics.h
+++ trabalho-pratico/include/entities/musics.h
@@ -1,7 +1,18 @@
 #ifndef MUSICS_H
 #define MUSICS_H
 
-typedef struct musics{
-}Musics;
+#include <glib.h>
+
+typedef struct music *Music;
+
+Music create_music(int id, char *t, GSList arts_ids, char *d, int d_s, char g, short year, char *l);
+
+int get_music_id(Music m);
+
+char get_genre(Music m);
+
+int get_duration(Music m);
+
+void free_music(Music m);
 
 #endif
--- trabalho-pratico/src/entities/musics.c
+++ trabalho-pratico/src/entities/musics.c
@@ -1,1 +1,58 @@
+#include <stdlib.h>
 #include "musics.h"
+
+typedef struct music {
+    int id;
+    char *title;
+    GSList artists_ids;
+    char *duration;
+    int duration_s;
+    char genre;
+    short year;
+    char *lyrics;
+} *Music;
+
+
+
+Music create_music(int id, char *t, GSList arts_ids, char *d, int d_s, char g, short y, char *l) {
+    Music m = malloc(sizeof(struct music));
+    m->id = id;
+    m->title = strdup(t);
+    m->artists_ids = arts_ids;
+    m->duration = strdup(d);
+    m->duration_s = d_s;
+    m->genre = g;
+    m->year = y;
+    m->lyrics = strdup(l);
+
+    return m;
+}
+
+
+
+int get_music_id(Music m) {
+    return (m->id);
+}
+
+
+
+char get_genre(Music m) {
+    return (m->genre);
+}
+
+
+
+int get_duration(Music m) {
+    return (m->duration);
+}
+
+
+
+void free_music(Music m) {
+    free(m->title);
+    free(m->duration);
+    free(m->lyrics);
+    g_slist_free_full(m->artists_ids, free);
+
+    free(m);
+}
