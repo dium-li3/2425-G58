e5b6c3513e1af3aec862f04eaf27206d3da8311b
a106857 <a106857@alunos.uminho.pt>
2024-11-02 19:29:45 +0000

    Atualização do comparador de ficheiros e gitignore para os outputs do cachegrind.

--- trabalho-pratico/test.c
+++ trabalho-pratico/test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/resource.h>
+#include <time.h>
 
 
 void update_paths(char *rf, char *ef, char *rd, char *ed, int i) {
@@ -10,16 +11,14 @@ void update_paths(char *rf, char *ef, char *rd, char *ed, int i) {
 }
 
 
-
 void close_files(FILE *p1, FILE *p2) {
     fclose(p1);
     fclose(p2);
 }
 
 
-
 /*
-    Retorna 0 se houver discrepâncias ou 1 se não houver. Isto para ir somando o nº de queries certas.
+    Retorna 0 se houver discrepâncias ou 1 se não houver.
 */
 int compare_files(FILE *rfp, FILE *efp) {
         char *line_r = NULL, *line_e = NULL;
@@ -37,15 +36,11 @@ int compare_files(FILE *rfp, FILE *efp) {
 }
 
 
-
-int main(int argc, char** argv) {
-    if(argc != 4) {
-        printf("Os argumentos são inválidos.\n");
-        return 1;
-    }
+void testagem(char *expected) {
+    printf("\n\n===TESTAGEM===\n\n\n");
 
     char *results_dir = strdup("resultados/");
-    char *expected_dir = strdup(argv[3]);
+    char *expected_dir = strdup(expected);
 
     char results_file[40] = {'\0'};
     char expected_file[50] = {'\0'};
@@ -68,7 +63,30 @@ int main(int argc, char** argv) {
     free(results_dir);
     free(expected_dir);
 
-    printf("Queries certas: %d/%d\n", corrects, i-1);
-    
+    printf("Queries certas: %d/%d\n\n", corrects, i-1);
+}
+
+
+
+int main (int argc, char **argv){
+    if(argc != 4) {
+        printf("Os argumentos são inválidos.\n");
+        return 1;
+    }
+
+    struct timespec start, end;
+    double elapsed;
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+
+    testagem(argv[3]);
+
+    struct rusage r_usage;
+    getrusage(RUSAGE_SELF, &r_usage);
+    printf("Utilização de memória: %ld KB\n\n", r_usage.ru_maxrss);
+    printf("Tempo total: %.6fs\n\n", elapsed);
+
     return 0;
 }
