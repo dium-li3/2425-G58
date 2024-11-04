ddbe242a461298e663148259e800ac52580747ac
lucasxdsy <79836549+lucasxdsy@users.noreply.github.com>
2024-11-04 16:02:13 +0000

    Update music_manager.h

--- trabalho-pratico/include/managers/music_manager.h
+++ trabalho-pratico/include/managers/music_manager.h
@@ -15,13 +15,15 @@ Music_Manager create_music_manager();
 
 Genre create_gen(char* gen_name);
 
-void gen_freq_acum(Music_Manager mm,int min_age, int max_age);
+void add_like_genre(Music_Manager mm, char *genre, short age);
+
+void gen_freq_acum(Music_Manager mm);
 
 int cmp_like_gen (gconstpointer g1, gconstpointer g2);
 
 void sort_gen(Music_Manager mm,int min_age, int max_age);
 
-void insert_gen(Music m, Music_Manager mus_m, int i);
+int insert_gen(Music m, Music_Manager mus_m, int i);
 
 void insert_music_by_id(Music m, Music_Manager music_manager);
 
@@ -35,6 +37,6 @@ void get_total_likes(Music_Manager mm,int min_age, int max_age);
 
 Genre get_genre_by_index(Music_Manager mm,int index);
 
-void print_genre_info(Genre gen, FILE *fp);
+int print_genre_info(Genre gen, FILE *fp);
 
 #endif
