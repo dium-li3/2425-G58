779b6564bbb964cd72c64de385071a3cbbe0a712
a106907 <a106907@alunos.uminho.pt>
2024-11-25 23:10:26 +0000

    Documentação da entidade Artista e do seu gestor.

--- trabalho-pratico/include/entities/artists.h
+++ trabalho-pratico/include/entities/artists.h
@@ -6,34 +6,223 @@
 
 #include "output.h"
 
+/**
+ * @brief Example showing how to document a function with Doxygen.
+ *
+ * Description of what the function does. This part may refer to the parameters
+ * of the function, like @p param1 or @p param2. A word of code can also be
+ * inserted like @c this which is equivalent to <tt>this</tt> and can be useful
+ * to say that the function returns a @c void or an @c int. If you want to have
+ * more than one word in typewriter font, then just use @<tt@>.
+ * We can also include text verbatim,
+ * @verbatim like this@endverbatim
+ * Sometimes it is also convenient to include an example of usage:
+ * @code
+ * BoxStruct *out = Box_The_Function_Name(param1, param2);
+ * printf("something...\n");
+ * @endcode
+ * Or,
+ * @code{.py}
+ * pyval = python_func(arg1, arg2)
+ * print pyval
+ * @endcode
+ * when the language is not the one used in the current source file (but
+ * <b>be careful</b> as this may be supported only by recent versions
+ * of Doxygen). By the way, <b>this is how you write bold text</b> or,
+ * if it is just one word, then you can just do @b this.
+ * @param param1 Description of the first parameter of the function.
+ * @param param2 The second one, which follows @p param1.
+ * @return Describe what the function returns.
+ * @see Box_The_Second_Function
+ * @see Box_The_Last_One
+ * @see http://website/
+ * @note Something to note.
+ * @warning Warning.
+ */
+
+/**
+ * @brief Estrutura que representa um artista, com todas as informações que lhe são associadas.
+ *
+ * Mais especificamente, é um pointer para uma estrutura.
+ */
+
 typedef struct artist *Artist;
 
-// Devolve o id de um artista.
+/**
+ * @brief Inicializa um artista.
+ *
+ * Função que cria propriamente um artista, utilizada quando este é validado na
+ * create_artist_from_tokens. O campo @c disc_duration será inicialmente posto 
+ * a zero, sendo somente preenchido durante a leitura das músicas. Convencionou-se,
+ * para o parâmetro @p type, um dos dois valores: @c 'I' para individual; @c 'G', para grupo.
+ * 
+ * @param id Id do artista.
+ * @param name Nome do artista.
+ * @param idc Array com os ids que constituem a entidade, caso seja do tipo @c "G".
+ * @param coun Country do artista.
+ * @param type Tipo do artista.
+ * @return Devolve um artista inicializado.
+ */
+
+Artist create_art(int id, char *name, double rps, GArray *idc, char *coun, char type)
+
+/**
+ * @brief Função desenvolvida para satisfazer o encapsulamento.
+ *
+ * Algoritmo necessário à componente de encapsulamento. 
+ * 
+ * @param a Artista cujo id será devolvido.
+ * @return Id do artista.
+ */
+
 int get_art_id(Artist a);
+
+/**
+ * @brief Função desenvolvida para satisfazer o encapsulamento.
+ *
+ * Algoritmo necessário à componente de encapsulamento. 
+ * 
+ * @param a Artista cujo id será devolvido.
+ * @return Pointer - para um inteiro - o qual aponta
+ * para o id do artista.
+ * @warning Esta função não é usada no código.
+ */
 
 int *get_art_id_pointer(Artist a);
 
-// Devolve a country de um artista.
+/**
+ * @brief Função desenvolvida para satisfazer o encapsulamento.
+ *
+ * Algoritmo necessário à componente de encapsulamento. 
+ * 
+ * @param a Artista cujo país será devolvido.
+ * @return País do artista recebido.
+ */
+
 const char *get_art_country(Artist a);
+
+/**
+ * @brief Função desenvolvida para satisfazer o encapsulamento.
+ *
+ * Algoritmo necessário à componente de encapsulamento. 
+ * 
+ * @param a Artista cujo tempo de discografia será devolvido.
+ * @return Tempo de discografia o país do artista recebido.
+ */
 
 int get_disc_duration(Artist a);
 
+/**
+ * @brief Função desenvolvida para satisfazer o encapsulamento.
+ *
+ * Algoritmo necessário à componente de encapsulamento. 
+ * 
+ * @param a Artista cujo nome será devolvido.
+ * @return Devolve o nome do artista recebido.
+ */
+
 const char *get_art_name(Artist a);
 
-// Dada uma string com o tipo do artista, devolve o caracter que o representa.
+/**
+ * @brief Dada uma string com o tipo do artista, devolve o caracter que o representa.
+ *
+ * A princípio, o artista tem o tipo @c 'E' (que representa "error"). O seu campo @c type
+ * será então comparado com as strings @c "individual" e @c "group", adquirindo o valor
+ * @c 'I' ou @c 'G' consoante se verifique uma das respetivas igualdades.
+ * 
+ * @param a Artista cujo país será devolvido.
+ * @return Devolve o país do artista recebido.
+ */
+
 char get_art_type(char *art_type);
+
+/**
+ * @brief Função desenvolvida para satisfazer o encapsulamento.
+ *
+ * Algoritmo necessário à componente de encapsulamento. 
+ * 
+ * @param a Artista cujo tipo será devolvido.
+ * @return Devolve o tipo do artista recebido.
+ * @warning Esta função não é usada no código.
+ */
 
 char get_art_type_from_art(Artist a);
 
+/**
+ * @brief Função que "printa" um artista.
+ *
+ * Gera um array de strings onde serão armazenadas as informações do artista. Ademais, recebe
+ * o tipo de caracter (correspondente ao parâmetro @p separador ) que a query exije que separe o output.
+ * 
+ * @param a Artista cujo país será devolvido.
+ * @param separador Carácter que vai separar os outputs de uma mesma linha.
+ * @param out Struct usado pelo output.
+ * @return Retorna @c void.
+ */
+
 void print_art_info(Artist a, char separador, Output out);
+
+/**
+ * @brief Função usada de base para ordenar o array de artistas do @b artist_manager
+ *
+ * É um dos parâmetros que a função da glib @b g_array_sort() precisa para ordenar
+ * o array de artistas do @b artist_manager segundo o tempo de discografia de cada um.
+ * Quando a função for invocada, os parâmetros @p a e @p b serão dois artistas a ser
+ * comparados.
+ * 
+ * A função retorna menor que zero se @p b for menor que @p a, zero se for igual,
+ * maior que zero se @p b for maior que @p a. É uma versão modificada do tipo
+ * qsort()-style, para que o array fique ordenado em ordem decrescente, facilitando
+ * a sua exposição posteriormente.
+ * 
+ * @param a um g_pointer para um artista constante.
+ * @param b outro artista que será comparadio com @p a.
+ * @return Resultado da comparação.
+ */
 
 int compare_dur (gconstpointer a, gconstpointer b);
 
+/**
+ * @brief Cria um artista se todas as suas informações forem válidas.
+ *
+ * Faz a validação sintática do artista, verificando se a lista dos
+ * artistas constituintes daquele id começa e termina com os caracteres
+ * @c "[" e @c "]", respectivamente. Somado a isso, também faz a validação lógica
+ * do artista, conferindo se o campo @c id_constituent de um artista 
+ * individual não possui elementos.
+ * 
+ * Satisfeitas essas checagens, a função cria um artista.
+ * 
+ * @param tokens Strings segmentadas que contêm os dados necessários para criar
+ * um artista.
+ * @return Resultado da comparação.
+ */
+
 Artist create_artist_from_tokens(char **tokens);
+
+/**
+ * @brief Adiciona uma duração ao tempo de discografia de um artista individual.
+ *
+ * Este algoritmo será posteriormente usado pela função @c add_dur_artists, cujo
+ * propósito é parecido, pois adiciona uma duração a todos os artistas que
+ * produziram uma determinada música.
+ * 
+ * @param a Artista cuja discografia será incrementada.
+ * @param duration Valor a incrementar na discografia.
+ * @return Retorna @c void.
+ */
 
 void add_disc_duration(Artist a, int duration);
 
-// Dá free do artista
+/**
+ * @brief Libera a memória ocupada por um registo de artista.
+ *
+ * "Dá free" separadamente de cada um dos campos do artista.
+ * 
+ * @param a Artistas cujos dados devem ser liberados.
+ * @return Retorna @c void.
+ */
+
 void free_art(Artist a);
 
 #endif
--- trabalho-pratico/include/managers/artist_manager.h
+++ trabalho-pratico/include/managers/artist_manager.h
@@ -9,23 +9,155 @@ typedef struct art_manager *Art_Manager;
 
 typedef struct array_art *Arr_art;
 
+/**
+ * @brief Inicializa um gestor de artistas.
+ *
+ * Função que retorna um gestor de artistas, constituído por
+ * uma hash table de artistas cuja chave são os seus ids, e
+ * um array com os mesmos artistas, que será ordenado de forma
+ * decrescente com base no tempo de discografia dos seus membros.
+ * Esta função é chamada no @b master_manager.
+ * 
+ * @return Gestor de artista, com sua hash e seu array vazios.
+ */
+
 Art_Manager create_art_manager();
+
+/**
+ * @brief Insere um artista na tabela hash do gestor de artistas
+ *
+ * Recebe o artista e o seu gestor, de forma a obter o id do artista
+ * com o @b get_art_id e inserir esse valor (como chave) e o artista
+ * em si na tabela hash do gestor. 
+ * 
+ * @param a Artista que será inserido na tabela.
+ * @param art_manager Gestor proprietário da tabela.
+ * @return Retorna @c void.
+ */
 
 void insert_artists_by_id(Artist a, Art_Manager art_manager); 
 
+/**
+ * @brief Ordena o array de artistas do gestor pelo tempo de discografia
+ * dos seus membros
+ *
+ * Após o armazenamento dos artistas e das músicas nos seus respetivos
+ * gestores, essa função ordena o array dos artistas (cada um com o campo
+ * @c disc_duration já preenchido) pelo seu tempo de discografia.
+ * 
+ * @param artist_manager Gestor cujo array será ordenado.
+ * @return Retorna @c void.
+ */
+
 void order_duration (Art_Manager artist_manager);
+
+/**
+ * @brief Preenche um gestor com todos os artistas que estão num ficheiro
+ * cujo path é dado.
+ *
+ * Função chamada pelo @b master_manager para guardar no gestor de artistas
+ * - especificamente no seu array e na sua hash - todos os artistas
+ * presentes em um ficheiro dado o caminho deste.
+ * 
+ * Ademais, o algoritmo também expõe algum artista que esteja incorreto.
+ * 
+ * @param art_path caminho do ficheiro que contém os artistas.
+ * @param artist_manager Gestor que será preenchido com os artistas do ficheiro.
+ * @return Retorna @c void.
+ */
 
 void store_Artists (char *art_path, Art_Manager artists_manager);
 
+/**
+ * @brief Confere se todos os artistas de um array estão presentes no gestor.
+ *
+ * Função que devolve @c TRUE se todos os artistas de um array se encontram
+ * no array do gestor de artistas.
+ * 
+ * Esta função é utilizada durante o armazenamento das músicas, assegurando
+ * que todos os artistas que produziram uma determinada música estão registados
+ * no gestor, para que a duração dessa música seja adicionada ao campo 
+ * @c disc_duration de cada um deles.
+ * 
+ * Ademais, o algoritmo também expõe algum artista que esteja incorreto.
+ * 
+ * @param artists Array dos artistas produtores de dada música.
+ * @param artist_manager Gestor usado de parâmetro.
+ * @return @c TRUE se todos os artistas se encontram no gestor; @c FALSE, do contrário.
+ */
+
 gboolean all_artists_exist (const GArray *artists, Art_Manager am);
+
+/**
+ * @brief Adiciona um intervalo de tempo à discografia de um grupo de artistas.
+ *
+ * Após a @b all_artists_exist confirmar a existência de todos os artistas que
+ * compuseram uma determinada música, será a @b add_dur_artists a responsável por
+ * adicionar, em cada um dos artistas, a duração dessa faixa aos seus campos
+ * @c disc_duration
+ * 
+ * @param music_artists Array dos artistas produtores de dada música.
+ * @param duration Valor que será adicionado ao tempo de discografia de cada membro
+ * do array.
+ * @param am Gestor de artistas.
+ * @return Retorna @c void.
+ */
 
 void add_dur_artists (const GArray *music_artists , int duration, Art_Manager am);
 
+/**
+ * @brief Expõe os N primeiros artistas do array do gestor, dado o seu country.
+ *
+ * Logo depois dos artistas estarem devidamente guardados no seu gestor, tendo o
+ * array deste já sido devidamente ordenado em função do tempo de discografia dos
+ * seus integrantes, a função percorre todo esse array e revela os seus @p N primeiros
+ * membros cujo país coincida com o @p country dado como parâmetro.
+ * 
+ * @param am Gestor que fornece o array de artistas.
+ * @param country país usado como filtro na busca.
+ * @param N Números de artistas a serem expostos.
+ * @param separador Carácter que vai separar os outputs de uma mesma linha.
+ * @param out Struct usado pelo output.
+ * do array. 
+ * @return Retorna @c void.
+ */
+
 void print_N_country_art_info (Art_Manager am, char *country, int N, char separador, Output out);
+
+/**
+ * @brief Expõe os N primeiros artistas do array do gestor.
+ *
+ * "Dá print" dos @p N primeiros artistas do array já ordenado do gestor.
+ * 
+ * @param am Gestor que fornece o array de artistas.
+ * @param N Números de artistas a serem expostos.
+ * @param separador Carácter que vai separar os outputs de uma mesma linha.
+ * @param out Struct usado pelo output.
+ * do array. 
+ * @return Retorna @c void.
+ */
 
 void print_N_art_info (Art_Manager am, int N, char separador, Output out);
 
+/**
+ * @brief Devolve o tamanho do array de artistas do gestor.
+ *
+ * Dado um gestor de artistas, devolve o tamanho do seu array de artistas.
+ * 
+ * @param am Gestor proprietário do array de artistas.
+ * @return O tamanho do array de artistas do gestor.
+ */
+
 int length_arr_disc (Art_Manager am);
+
+/**
+ * @brief Libera a memória ocupada por um gestor de artistas.
+ *
+ * "Dá free" separadamente de cada um dos campos do gestor de artistas.
+ * 
+ * @param art_manager Gestor cujos dados devem ser liberados.
+ * @return Retorna @c void.
+ */
 
 void free_art_manager(Art_Manager art_manager);
 
